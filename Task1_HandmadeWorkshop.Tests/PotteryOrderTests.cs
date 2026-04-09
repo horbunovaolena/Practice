83de7a06254ca8d06523f644f8a39fda7cdb07ba
horbunovaolena <horbunovaelenapetrovna@gmail.com>
2026-04-09 16:24:14 +0300

    Add unit tests for PotteryOrder and test project setup
    Added Task1_HandmadeWorkshop.Tests project with xUnit and coverlet for testing and coverage. Implemented PotteryOrderTests for price calculation logic. Updated solution file to include the test project. Fixed minor formatting issue in Program.cs.

---
+++ Task1_HandmadeWorkshop.Tests/PotteryOrderTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1_HandmadeWorkshop.Tests
+{
+    public class PotteryOrderTests
+    {
+        [Fact]
+        public void CalculateFinalPrice_VolumeIsBiggerThan2Liters_PriceMustBe300More()
+        {
+            // Arrange
+            const decimal basePrice = 1500m;
+            var potteryOrder = new PotteryOrder("Глина", "Полтава", 6, basePrice, 2.5m);
+
+            // Act
+            var finalPrice = potteryOrder.CalculateFinalPrice();
+
+            // Assert
+            Assert.Equal(1800m, finalPrice);
+        }
+
+        [Fact]
+        public void CalculateFinalPrice_DaysIsLessThanUrgent_PriceMustBeMultipliedByTheUrgentPriceMuktiplier()
+        {
+            // Arrange
+            const decimal basePrice = 1000m;
+            var potteryOrder = new PotteryOrder("Глина", "Полтава", 2, basePrice, 1m);
+
+            // Act
+            var finalPrice = potteryOrder.CalculateFinalPrice();
+
+            // Assert
+            Assert.Equal(basePrice * 1.2m, finalPrice);
+        }
+    }
+}
